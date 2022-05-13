137d760d827aa8b480b0e5ca363d07ab3fcac56c
MennaLotfy <mennalotfy1111@gmail.com>
2022-05-13 23:25:22 +0200

    beef kilos check and calculate timer

--- Beef-Chicken.c
+++ Beef-Chicken.c
@@ -69,4 +69,49 @@ if ((x== '1')||(x== '2')||(x== '3')||(x== '4')||(x== '5')	||(x== '6')||(x== '7')
   x=Keypad_Getkey();	
   kilos_check_chicken();
 }
+
+
+
+void kilos_check_Beef (void)
+	{
+	     int int_no_of_kilos=0;
+		   float timer =0 ;
+       no_of_kilos=itoa(0,buffer,10);
+		   m=0;
+		   s=0;
+if ((x== '1')||(x== '2')||(x== '3')||(x== '4')||(x== '5')	||(x== '6')||(x== '7')||(x== '8')||(x== '9')) //integer from 1 to 9
+		{
+			lcd_cmd(lcd_Clear);
+			lcd_gotoxy(6,1);
+			lcd_write(x) ; 
+			lcd_write_string("kg");
+			no_of_kilos[0]=x;
+			int_no_of_kilos=atoi(no_of_kilos);
+			timer=(int_no_of_kilos*30) ; //30 seconds 
+			no_of_kilos=itoa(timer,buffer,10);
+			Systick_n10ms(200);
+			
+			 while (timer>=60)
+			 { 
+				 m=m+1;
+				 timer=timer-60;
+			 }
+			 s=timer;
+			counts_min_sec(m,s);
+			 
+	}
+		
+		else 
+		{
+			lcd_cmd(0x01);
+			lcd_write_string("       Err");
+			Systick_n10ms(200);
+			lcd_cmd(lcd_Clear);
+			lcd_write_string("Beef weight:");
+      x=Keypad_Getkey();
+			kilos_check_Beef();
+		}
+
+	};
+
 	

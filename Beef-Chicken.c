b9c1d3b7c793a81e82e121a6a5fa864442f27c79
MennaLotfy <mennalotfy1111@gmail.com>
2022-05-13 23:19:07 +0200

    Initialization

--- Beef-Chicken.c
+++ Beef-Chicken.c
@@ -7,3 +7,10 @@
 #include "KPD.h"
 #include "GPIO_INIT.h"
 #include "functions.h"
+
+extern int s;
+extern int m;
+char N_K[3]={'0','0',0};
+char * no_of_kilos=N_K;
+char x='I'; //keypad input
+char buffer[33];

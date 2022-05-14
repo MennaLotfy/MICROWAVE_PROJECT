b6b88461536d0c68a56aac3e83d26f1498548bbe
MennaLotfy <mennalotfy1111@gmail.com>
2022-05-14 14:58:55 +0200

    comment

---
+++ functions.d
@@ -0,0 +1,9 @@
+.\functions.o: functions.c
+.\functions.o: C:\Keil\ARM\Inc\TI\TM4C123\tm4c123gh6pm.h
+.\functions.o: C:\Keil\ARM\CMSIS\Include\core_cm4.h
+.\functions.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\functions.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\functions.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\functions.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
+.\functions.o: C:\Keil\ARM\Inc\TI\TM4C123\system_TM4C123.h
+.\functions.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h

6e08a78b4e8b73e8faaaee2c1f69735afbe2571a
hadygamal <hadygamal1@gmail.com>
2022-05-15 00:54:36 +0200

    Update Option-D.c

--- Option-D.c
+++ Option-D.c
@@ -5,4 +5,41 @@
 #include "LCD.h"
 #include "stdlib.h"
 #include "GPIO_INIT.h"
-#define Delay_Value 160000
+#include "functions.h"
+
+void Option_D()
+{
+	uint32_t take_cooking_time_return,SW;
+	lcd_cmd(lcd_Clear);
+	lcd_cmd(first_line1);
+	Vaild:
+	lcd_write_string("Cooking Time?");
+	take_cooking_time_return=take_cooking_time();
+	if(take_cooking_time_return==1)
+	{
+		while(1)
+		{
+			if((Read_SW()&0x10)==0x00)
+			{
+				GPIO_PORTF_DATA_R|=Red;
+				lcd_cmd(lcd_Clear);
+			}
+			if((Read_SW()&0x01)==0x00)
+			{
+				GPIO_PORTF_DATA_R|=Blue;
+				//start timer
+				lcd_write_string("  Cooking Done  ");
+				break;
+			}
+		}
+	}
+	
+	else if(take_cooking_time_return==0)
+	{
+		lcd_cmd(lcd_Clear);
+ 	  lcd_write_string("Enter valid time");
+		delay_ms(6000);
+		lcd_cmd(lcd_Clear);
+		goto Vaild;
+	}
+}
